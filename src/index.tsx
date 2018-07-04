fde059907bc23db31ed89529cec0db11a382a75c
alonso.torres <alonso.torres@kaleidos.net>
2018-07-04 17:07:47 +0200

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import { render } from "react-dom";
+import { AppContainer } from "react-hot-loader";
+
+import RootComponent from "./ui/RootComponent";
+
+const ROOT_ELEMENT = document.getElementById("app-root");
+
+function renderApp(Component) {
+  render(
+    <AppContainer>
+      <Component/>
+    </AppContainer>,
+    ROOT_ELEMENT,
+  );
+}
+
+renderApp(RootComponent);
+
+// Hot Module Replacement API
+declare let module: { hot: any };
+
+if (module.hot) {
+  module.hot.accept("./ui/RootComponent", () => {
+    const NewRootComponent = require("./ui/RootComponent").default;
+    renderApp(NewRootComponent);
+  });
+}
---
+++ webpack/dev.js
@@ -0,0 +1,37 @@
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+const common = require('./common');
+
+module.exports = merge(common, {
+  mode: 'development',
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:8081',
+    'webpack/hot/only-dev-server',
+    './index.tsx'
+  ],
+  devServer: {
+    hot: true,
+    stats: {
+      colors: true,
+      hash: false,
+      version: false,
+      timings: false,
+      assets: false,
+      chunks: false,
+      modules: false,
+      reasons: false,
+      children: false,
+      source: false,
+      errors: true,
+      errorDetails: false,
+      warnings: true,
+      publicPath: false
+    }
+  },
+  devtool: 'cheap-module-eval-source-map',
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+  ],
+});
